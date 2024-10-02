5065b0a1d8cfa6cf8dfc71efb5e9900452651b52
louismineo <50332624+louismineo@users.noreply.github.com>
2024-10-02 11:13:52 +0800

    creating new files for migration and etc

---
+++ server/seeders/20241001095806-create-departments.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('departments', [
+      {
+        deptId: 1,
+        name: 'Admin'
+      },
+      {
+        deptId: 2,
+        name: 'PS'
+      },
+      {
+        deptId: 3,
+        name: 'HR'
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
